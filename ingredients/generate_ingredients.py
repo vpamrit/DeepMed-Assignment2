0427b3259cec721a857b66f2c60e443f30ed9c17
vpamrit <vishnuamritpydah@gmail.com>
2020-03-09 16:46:54 -0400

    updating ingredients list orderings

--- ingredients/generate_ingredients.py
+++ ingredients/generate_ingredients.py
@@ -3,13 +3,13 @@ import csv
 out_filename = './ingredients.csv'
 categories = ['model_name',  'model_number', 'model_type', 'target_classes', 'balance', 'epoch_size']
 rows = [
-    ['resnet', 101, '1vs1', '0,1', 0.99, 1200],
-    ['resnet', 101, '1vs1', '1,4', 0.99, 1200],
     ['resnet', 101, 'All', '0,1,2,3,4,5,6', 0.85, 5000],
     ['resnet', 101, 'Most', '0,2,3,4,5,6', 0.90, 3500],
+    ['resnet', 101, '1vs1', '0,1', 0.99, 1200],
+    ['resnet', 101, '1vs1', '1,4', 0.99, 1200],
     ['resnet', 101, '1vsMany', '1,others', 0.95, 2500],
     ['resnet', 101, '1vsMany', '4,others', 0.85, 1000],
-    ['resnet', 101, '1vsMany', '0,others', 0.85, 1000]
+    ['resnet', 101, '1vsMany', '0,others', 0.85, 1000],
 ]
 
 with open(out_filename, 'w', newline='') as outfile:
