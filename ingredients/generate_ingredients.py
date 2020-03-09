0e6ab9bc50e9c5e7c8c60220ce6487fe0f94774e
vpamrit <vishnuamritpydah@gmail.com>
2020-03-08 23:00:46 -0400

    added updated ingredients

--- ingredients/generate_ingredients.py
+++ ingredients/generate_ingredients.py
@@ -3,9 +3,11 @@ import csv
 out_filename = './ingredients.csv'
 categories = ['model_name',  'model_number', 'model_type', 'target_classes', 'balance', 'epoch_size']
 rows = [
-    ['resnet', 101, '1toMany', '1,others', 0.95, 2500],
-    ['resnet', 50, '1to1', '0,1', 0.99, 1200],
-    ['resnet', 50, '1to1', '1,4', 0.99, 1200],
+    ['resnet', 101, '1vsMany', '1,others', 0.95, 2500],
+    ['resnet', 101, '1vsMany', '4,others', 0.80, 1000],
+    ['resnet', 101, '1vsMany', '0,others', 0.80, 1000],
+    ['resnet', 50, '1vs1', '0,1', 0.99, 1200],
+    ['resnet', 50, '1vs1', '1,4', 0.99, 1200],
     ['resnet', 101, 'All', '0,1,2,3,4,5,6', 0.85, 5000],
     ['resnet', 101, 'Most', '0,2,3,4,5,6', 0.90, 3500]
 ]
